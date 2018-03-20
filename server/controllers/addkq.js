36a63bedf011bc86744c5b9faf2a7172fc21ef05
lbpstar <lbpstar@gmail.com>
2018-03-20 23:21:14 +0800

    20180320

--- server/controllers/addkq.js
+++ server/controllers/addkq.js
@@ -1,17 +1,13 @@
 const { mysql } = require('../qcloud')
-
-
-  
-  //mysql('user').insert({ fid: '2', open_id: '2', fname: '1', phone_no: '2' })
-  //console.log(`执行了`)
 module.exports = async function (ctx, next) {
-    //ctx.state.data = { msg: 'Hello World' }
-    //mysql('user').insert({ fid: '2', open_id: '2', fname: '1', phone_no: '2' })
-    //ctx.state.data = mysql.select().table('users')
-  //await mysql.select('fname').from('user').then(res => {
-  await mysql('user').insert({ fid: '2', open_id: '2', fname: '1', phone_no: '2' }).then(res => {
-    ctx.state.data = res;
-  });
+  if (ctx.state.$wxInfo.loginState) {
+    ctx.state.data = ctx.state.$wxInfo.userinfo
+    ctx.state.data['time'] = Math.floor(Date.now() / 1000)
+    //await mysql('kaoqin').insert({ fid: '2', open_id: '2', fname: '1', phone_no: '2' }).then(res => {
+      //ctx.state.data = res;
+    //});
+  }
+
   }
 
 
