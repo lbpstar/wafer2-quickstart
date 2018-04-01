648ffe8aa5ef46011e923507815a03b62b7de93c
lbpstar <lbpstar@gmail.com>
2018-04-01 20:19:01 +0800

    已经向数据库中插入openid

--- server/controllers/addkq.js
+++ server/controllers/addkq.js
@@ -1,18 +1,16 @@
 const { mysql } = require('../qcloud')
 module.exports = async (ctx, next) => {
-  //if (ctx.state.$wxInfo.loginState) {
-  //ctx.state.data = ctx.state.$wxInfo.userinfo
-  //ctx.state.data['time'] = Math.floor(Date.now() / 1000)
-  //await mysql('kaoqin').insert({ fid: '2', open_id: '2', fname: '1', phone_no: '2' }).then(res => {
-  //ctx.state.data = res;
-  //});
-  //}
+  
   if (ctx.state.$wxInfo.loginState === 1) {
     // loginState 为 1，登录态校验成功
-    ctx.state.data = ctx.state.$wxInfo.userinfo
+    ctx.state.data = ctx.state.$wxInfo.userinfo;
+  //ctx.state.data['time'] = Math.floor(Date.now() / 1000)
   } else {
     ctx.state.code = -1
   }
+  await mysql('user').insert({ fid: '5', open_id: ctx.state.$wxInfo.userinfo.openId, fname: '1', phone_no: '2' }).then(res => {
+    // ctx.state.data = res;
+  });
 }
 
 
